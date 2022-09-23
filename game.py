15338c8dabd904b552844a070b71e09e3fb91f17
KEI_BNPC <gzhongtian845@gmail.com>
2022-09-24 01:01:12 +0900

    壁チェック、描画アイテム追加

--- game.py
+++ game.py
@@ -18,6 +18,7 @@ TILE_SIZE = js_config["MAP"]["tile_size"]
 PLAYER_HP = js_config["PLAYER"]["hp"]
 PLAYER_DISTANCE = js_config["PLAYER"]["moving_distance"]
 
+btnp_frame = 5
 
 def update(self):
 
@@ -32,47 +33,93 @@ def draw(self):
 
 def drawing_map(self):
 
+    global map_tips
     for i in range(MAP_HEIGHT):
         for j in range(MAP_WIDTH):
-            pu.rect(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, 7, 0)
+            # 得点 小(ピクセル)
+            if map_tips[i][j] == 1:
+                px.rect(i * TILE_SIZE + 12, j * TILE_SIZE + 10, 5, 5, 5)
+            # 壁(レンガ)
+            if map_tips[i][j] == 9:
+                px.blt(i * TILE_SIZE, j * TILE_SIZE, 2, 96, 0, 32, 32, 0)
+            # 得点 大(カボチャ)
+            if map_tips[i][j] == 5:
+                px.blt(i * TILE_SIZE, j * TILE_SIZE + 2, 2, 0, 0, 32, 24, 7)
 
 
 def init(self):
 
+    global map_tips
+
     px.load("game.pyxres")
     self.player = Player()
-    self.map_tips = js_config["map_tips"]
+    map_dates = json.load(open("map_tips.json", "r"))
+    map_tips = map_dates["map_tips"]
+    map_tips = np.array(map_tips)
+    map_tips = np.rot90(map_tips, -3)
+    map_tips = np.flipud(map_tips)
 
 
 class Player:
     def __init__(self) -> None:
 
-        self.x = TILE_SIZE*15
-        self.y = TILE_SIZE*15
-        self.dx = 0
-        self.dy = 0
+        self.x = 15
+        self.y = 15
+        self.score = 0
+        self.nx = self.x
+        self.ny = self.y
         self.hp = PLAYER_HP
+        self.is_move = True
         self.is_alive = True
-        self.is_walking = 0
-        self.moveing_num = 0
         self.player_facing = 0
-        self.moving_distance = PLAYER_DISTANCE
 
     def update(self) -> None:
 
-        if px.btnp(px.KEY_UP) or px.btnp(px.KEY_W):
-            self.y -= self.moving_distance
+        global map_tips
+        if px.btnp(px.KEY_UP,btnp_frame,btnp_frame) or px.btnp(px.KEY_W,btnp_frame,btnp_frame):
+            self.ny -= 1
+            if self.can_move() == False:
+                return
             self.player_facing = 3
-        elif px.btnp(px.KEY_DOWN) or px.btnp(px.KEY_S):
+        elif px.btnp(px.KEY_DOWN,btnp_frame,btnp_frame) or px.btnp(px.KEY_S,btnp_frame,btnp_frame):
+            self.ny += 1
+            if self.can_move() == False:
+                return
             self.player_facing = 0
-            self.y += self.moving_distance
-        elif px.btnp(px.KEY_LEFT) or px.btnp(px.KEY_A):
+        elif px.btnp(px.KEY_LEFT,btnp_frame,btnp_frame) or px.btnp(px.KEY_A,btnp_frame,btnp_frame):
+            self.nx -= 1
+            if self.can_move() == False:
+                return
             self.player_facing = 1
-            self.x -= self.moving_distance
-        elif px.btnp(px.KEY_RIGHT) or px.btnp(px.KEY_D):
+        elif px.btnp(px.KEY_RIGHT,btnp_frame,btnp_frame) or px.btnp(px.KEY_D,btnp_frame,btnp_frame):
+            self.nx += 1
+            if self.can_move() == False:
+                return
             self.player_facing = 2
-            self.x += self.moving_distance
+        print(self.score)
+
+    def can_move(self):
+
+        if map_tips[self.nx][self.ny] == 9:
+            self.nx = self.x
+            self.ny = self.y
+            return False
+
+        self.x = self.nx
+        self.y = self.ny
+        self.score += map_tips[self.x][self.y]
+        map_tips[self.x][self.y] = 0
+        return True
 
     def draw(self) -> None:
 
-        px.blt(self.x, self.y, 0, 0, self.player_facing * 32, 32, 32, 5)
+        px.blt(
+            self.x * TILE_SIZE,
+            self.y * TILE_SIZE,
+            0,
+            0,
+            self.player_facing * 32,
+            32,
+            32,
+            5,
+        )
